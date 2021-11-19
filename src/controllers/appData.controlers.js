4f015302b690fd9e655299238f2bad1faf33cbaa
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-19 10:54:48 +0530

    adding file

---
+++ src/controllers/appData.controlers.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const AppData = require("../models/appData.model");
+const router = express.Router();
+
+router.post("/appData", async (req, res) => {
+    try {
+        let appData = new AppData(req.body);
+        appData = await appData.save();
+        res.status(200).json({
+            status: 200,
+            data: appData,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+router.get("/appData", async (req, res) => {
+    try {
+        let users = await AppData.find();
+        res.status(200).json({
+            status: 200,
+            data: users,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+router.get("/appData/:appDataId", async (req, res) => {
+    try {
+        let appData = await AppData.findOne({
+            _id: req.params.appDataId,
+        });
+        if (appData) {
+            res.status(200).json({
+                status: 200,
+                data: appData,
+            });
+        }
+        res.status(400).json({
+            status: 400,
+            message: "No appData found",
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+router.put("/appData/:appDataId", async (req, res) => {
+    try {
+        let appData = await AppData.findByIdAndUpdate(req.params.appDataId, req.body, {
+            new: true,
+        });
+        if (appData) {
+            res.status(200).json({
+                status: 200,
+                data: appData,
+            });
+        }
+        res.status(400).json({
+            status: 400,
+            message: "No appData found",
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+router.delete("/appData/:appDataId", async (req, res) => {
+    try {
+        let appData = await AppData.findByIdAndRemove(req.params.appDataId);
+        if (appData) {
+            res.status(200).json({
+                status: 200,
+                message: "AppData deleted successfully",
+            });
+        } else {
+            res.status(400).json({
+                status: 400,
+                message: "No appData found",
+            });
+        }
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+module.exports = router;
+
+
